7634360cb9431df11628ab24415d3826cf3887f6
Mitsuya.Watanabe <watanabe@gocco.co.jp>
2018-10-10 17:19:24 +0900

    Add 7 different blocks

--- pages/index.vue
+++ pages/index.vue
@@ -16,8 +16,8 @@
 export default {
   data () {
     return {
-      cols: 10,
-      rows: 15,
+      cols: 10 + 2,             // 左右に壁をつける為に +2 する
+      rows: 15 + 1,             // 下端に壁をつけるために +1 する
       cells: [],
       top: 0,
       top0: 0,                  // 1マス分ドロップした際に、前回描画したブロックをクリアする為に使用する
@@ -27,44 +27,92 @@ export default {
       dropInterval: 1000,
       dropSpeed: 50,            // 1秒間に操作できる最大回数
       tick: 0,
-      angles: [],               // 棒の回転後の位置を保持する
       angle: 0,                 // 現在の回転
-      parts: [],
-      parts0: []
+      angle0: 0,                // 前回の回転
+      parts0: [],
+      fills: [],                // ブロックのあるマス目を記録する
+      blocks: [],               // 表示させるブロックの数
+      block: {}
     }
   },
   mounted () {
     this.cells = document.getElementsByTagName('td')
+    // 壁をつくる
+    this.makeWalls()
+    // 落ちて来るブロックをつくる
+    this.blocks = this.getBlocks()
+
+    this.block = this.blocks[Math.floor(Math.random() * this.blocks.length)]
     document.onkeydown = this.keydown
     this.top = 1
     this.top0 = this.top
-    this.left = Math.floor(this.cols / 2)
-    this.left0 = this.left
+    this.left = Math.floor(this.cols / 2) // 青マスがちょうど中央に来るようにする
     this.dropInterval = 1000 / this.dropSpeed
-    this.angles = this.getAngles()
     this.move()
   },
   methods: {
-    getAngles: function() {
-      var w = this.cols
-      // 各々ブロック要素の相対位置を保持
-      // それぞれ、90度回転、180度回転、270度回転の順。
-      return [[-1, 1, 2], [-w, w, w * 2], [-2, -1, 1], [-w * 2, -w, w]]
+    getBlocks: function () {
+      const w = this.cols
+      return [
+        {
+          color: 'cyan',
+          angles:[[-1, 1, 2], [-w, w, w * 2], [-2, -1, 1], [-w * 2, -w, w]]
+        },
+        {
+          color: 'yellow',
+          angles:[[-w - 1, -w, -1]]
+        },
+        {
+          color: 'green',
+          angles:[[-w, 1 - w, -1], [-w, 1, w + 1], [1, w - 1, w], [-w - 1, -1, w]]
+        },
+        {
+          color: 'red',
+          angles:[[-w - 1, -w, 1], [1 - w, 1, w], [-1, w, w + 1], [-w, -1, w - 1]]
+        },
+        {
+          color: 'blue',
+          angles:[[-w - 1, -1, 1], [-w, 1 - w, w], [-1, 1, w + 1], [-w, w - 1, w]]
+        },
+        {
+          color: 'orange',
+          angles:[[1 - w, -1, 1], [-w, w, w + 1], [-1, 1, w - 1], [-w - 1, -w, w]]
+        },
+        {
+          color: 'magenda',
+          angles:[[-w, -1, 1], [-w, 1, w], [-1, 1, w], [-w]]
+        }
+      ]
+    },
+    makeWalls: function () {
+      for (let y = 0; y < this.rows; y++) {
+        for (let x = 0; x < this.cols; x++) {
+          if (x === 0 || x === this.cols - 1 ||
+              y === this.rows - 1) {
+                const pos = y * this.cols + x
+                this.cells[pos].style.backgroundColor = 'silver'
+                this.fills[pos] = 'silver'
+          }
+        }
+      }
     },
     keydown: function(e) {
       switch (e.keyCode) {
-        case 37:
+        case 37: // 左矢印キー
           this.keys.left = true
           break;
 
-        case 39:
+        case 39: // 右矢印キー
           this.keys.right = true
           break;
 
-        case 32:
+        case 32:　// スペースキー
           this.keys.rotate = true
           break;
 
+        case 40: // 下矢印キー
+          this.keys.down = true
+          break;
         default:
 
       }
@@ -74,46 +122,80 @@ export default {
       this.tick++
 
       this.left0 = this.left
-
-      if (this.keys.left && this.left > 0) {
-        this.left--
-      }
-
-      if (this.keys.right && this.left + 4 < this.cols) {
-        this.left++
-      }
-
-      if (this.keys.rotate) {
-        this.angle++
-      }
-
-      this.keys = {}
-      this.parts = this.angles[this.angle % this.angles.length]
-      for (let i = -1; i < this.parts0.length; i++) {
-        // 回転後の位置を取り出す
-        let offset0 = this.parts0[i] || 0
-
-        let index0 = this.top0 * this.cols + this.left0 + offset0
-        this.cells[index0].style.backgroundColor = ''
-
-        let offset = this.parts[i] || 0
-        let index = this.top * this.cols + 0 + this.left + offset
-        this.cells[index].style.backgroundColor = 'red'
-      }
-
       this.top0 = this.top
-      this.parts0 = this.parts
+      this.angle0 = this.angle
 
       if (this.tick % this.dropSpeed == 0) {
         this.top++
+      } else {
+        if (this.keys.left) {
+          this.left--
+        }
+
+        if (this.keys.right) {
+          this.left++
+        }
+
+        if (this.keys.rotate) {
+          this.angle++
+        }
+
+        if (this.keys.down) {
+          this.top++
+        }
       }
+
+      this.keys = {}
+
+      let parts = this.block.angles[this.angle % this.block.angles.length]
+
+      // NOTE: 当たり判定。壁にぶつかっていれば、前回の状態に戻す
+      for (let i = -1; i < parts.length; i++) {
+        const wallOffset = parts[i] || 0
+        const wallIndex = this.top * this.cols + 0 + this.left + wallOffset
+        if (this.fills[wallIndex]) {
+          if (this.tick % this.dropSpeed === 0) {
+            for (let j = -1; j < this.parts0.length; j++) {
+              const wallOffset0 = this.parts0[j] || 0
+              const wallIndex0 = this.top0 * this.cols + this.left0 + wallOffset0
+              this.fills[wallIndex0] = this.block.color
+            }
+
+            this.block = this.blocks[Math.floor(Math.random() * this.blocks.length)]
+            this.left0 = this.left = Math.floor(this.cols / 2)
+            this.top0 = this.top = 2
+            this.angle0 = this.angle = 0
+            this.parts0 = parts = this.block.angles[this.angle % this.block.angles.length]
+          } else {
+            this.left = this.left0
+            this.top = this.top0
+            this.angle = this.angle0
+            parts = this.parts0
+          }
+          // break
+        }
+      }
+
+      for (let i = -1; i < this.parts0.length; i++) {
+        // NOTE: 前回の位置の色を消す
+        // 回転後の位置を取り出す
+        let offset0 = this.parts0[i] || 0
+        let index0 = this.top0 * this.cols + this.left0 + offset0
+        this.cells[index0].style.backgroundColor = ''
+
+        // // NOTE: 現在の位置に色を塗る
+        let offset = parts[i] || 0
+        let index = this.top * this.cols + 0 + this.left + offset
+        this.cells[index].style.backgroundColor = this.block.color
+      }
+
+      this.top0 = this.top
+      this.parts0 = parts
 
       const info = this.tick + ' (' + this.left + ', ' + this.top + ')'
-      console.log(info);
+      // console.log(info);
 
-      if (this.top < this.rows) {
-        window.setTimeout(this.move, this.dropInterval)
-      }
+      window.setTimeout(this.move, this.dropInterval)
     }
   }
 }
