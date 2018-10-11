61124dd9578a685d4b1f6dee5e142ef5c8f056cd
Mitsuya.Watanabe <watanabe@gocco.co.jp>
2018-10-11 12:17:23 +0900

    Finish demo

--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,8 @@ export default {
       parts0: [],
       fills: [],                // ブロックのあるマス目を記録する
       blocks: [],               // 表示させるブロックの数
-      block: {}
+      block: {},
+      score: 0
     }
   },
   mounted () {
@@ -160,6 +161,42 @@ export default {
               const wallIndex0 = this.top0 * this.cols + this.left0 + wallOffset0
               this.fills[wallIndex0] = this.block.color
             }
+            
+            // NOTE: ブロックを消す
+            let cleans = 0
+            // NOTE: 下から走査して調べていく
+            for (let y = this.rows - 2; y >= 0; y--) {
+              let isFilled = true
+              for (let x = 1; x < this.cols - 1; x++) {
+                // NOTE: 一つでも塗られていないマスがあれば、ブレーク
+                if (!this.fills[y * this.cols + x]) {
+                  isFilled = false
+                  break
+                }
+              }
+
+              // NOTE: 1列全てマスが塗られている時の処理
+              if (isFilled) {
+                for (let y2 = y; y2 >= 0; y2--) {
+                  for (let x = 1; x < this.cols - 1; x++) {
+                    this.fills[y2 * this.cols + x] = this.fills[(y2 - 1) * this.cols + x]
+                  }
+                }
+
+                // NOTE: y--を相殺する為に追加する
+                y++
+                cleans++
+              }
+            }
+            if (cleans > 0) {
+              this.score += Math.pow(10, cleans) * 10
+              for (let y = this.rows - 2; y >= 0; y--) {
+                for (let x = 1; x < this.cols - 1; x++) {
+                  let color = this.fills[y * this.cols + x] || ''
+                  this.cells[y * this.cols + x].style.backgroundColor = color
+                }
+              }
+            }
 
             this.block = this.blocks[Math.floor(Math.random() * this.blocks.length)]
             this.left0 = this.left = Math.floor(this.cols / 2)
@@ -172,8 +209,12 @@ export default {
             this.angle = this.angle0
             parts = this.parts0
           }
-          // break
+          break
         }
+      }
+
+      if (this.top != this.top0) {
+        this.score++
       }
 
       for (let i = -1; i < this.parts0.length; i++) {
@@ -192,8 +233,9 @@ export default {
       this.top0 = this.top
       this.parts0 = parts
 
-      const info = this.tick + ' (' + this.left + ', ' + this.top + ')'
+      // const info = this.tick + ' (' + this.left + ', ' + this.top + ')'
       // console.log(info);
+      console.log(this.score);
 
       window.setTimeout(this.move, this.dropInterval)
     }
