29c497d4011772f7cdeb33224e84ade4cb27bcee
Mitsuya.WATANABE <watanabe@gocco.co.jp>
2018-12-22 14:23:47 +0900

    Fix favicon setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,9 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'favicon-16x16.png' }      
+      { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: './favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: './favicon-16x16.png' }      
     ],
     script: [
       { src: 'https://unpkg.com/vue-p5@0.7.x' }
